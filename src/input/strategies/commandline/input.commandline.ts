866fcd3e12dd2535caf9b29de65bc232d5792c00
Nitin Mukesh <nitin.mukesh@sine.co>
2022-02-15 23:30:30 +1030

    removed redundant code from input.commandline

--- src/input/strategies/commandline/input.commandline.ts
+++ src/input/strategies/commandline/input.commandline.ts
@@ -1,25 +1,18 @@
 import readline from 'readline';
-import createLogger from '../../../logger';
 import { ProcessorStrategy } from '../../../processor/processor.types';
 import { InputStrategyType, InputStrategy } from '../../input.types';
 
-const logger = createLogger('input.commandline');
 let rl: readline.Interface;
 
 const commandlineStrategy: InputStrategy = {
   type: InputStrategyType.COMMAND_LINE,
-  readInput: async (processor: ProcessorStrategy) => {
+  readInput: (processor: ProcessorStrategy) => {
     rl = readline.createInterface({
       input: process.stdin,
       output: undefined,
     });
-    try {
-      rl.on('line', processor.processUserAction);
-      rl.question('', processor.processUserAction);
-    } catch (error) {
-      logger.error('Failed to attached listener to readline', error);
-      throw error;
-    }
+    rl.on('line', processor.processUserAction);
+    rl.question('', processor.processUserAction);
   },
   closeInput: () => {
     return rl.close();
